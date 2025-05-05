de80285800973ffd84b32579dc082e43a66c037f
Jens Meindertsma <mail@jensmeindertsma.com>
2025-05-05 20:52:01 +0200

    Implement first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    print!("$ ");
+
+    io::stdout().flush().unwrap();
+
+    // Wait for user input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
 }
